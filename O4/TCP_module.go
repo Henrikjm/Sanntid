564b64a5b1bf83bef62fc29a59db486e7b45652d
student <student@Sanntid.(none)>
2014-02-06 14:11:19 +0100

    baesj

--- O4/TCP_module.go
+++ O4/TCP_module.go
@@ -2,6 +2,7 @@
 
 package main
 
+
 import(
 	. "net"
 	. "fmt"
@@ -9,32 +10,37 @@ import(
 )
 
 
-
 func TCP_hookup( adress string) Conn {
 	conn, err := Dial("tcp", adress)
-	if err != nil {
-		//Handle connection error
-		Println("Connection Error")
-	}
-	//Println("Connection succesfull: ", adress)
+	checkError(err, "Dial Error")
 	return conn
 }
 
-/*
-func TCP_listener( port string ) Conn {
-	listener, err := Listen("tcp", port )
-	if err != nil{
-		//Handle listener error
-		Println("Listener error")
-	}
-	channel, err := listener.Accept()
-		if err != nil{
-		//Handle listener accept error
-		Println("Listener Accept Error")
-	}
-*/ 
 
-	func main(){
+func TCP_listener( port string ) Conn {
+	port = ":" + port
+	listener, err := Listen("tcp", port)
+	conn, err := listener.Accept()
+	checkError(err, "Listen Error")
+	return conn
+}
+
+
+func checkError(err error, errorMsg string) {
+	if err != nil {
+		Println("!!Error type: " + errorMsg)
+	}
+}
+
+
+func writeMsg(conn Conn, msg string) {
+	data := []byte(msg)
+	_, err = conn.Write(data)
+	checkError(err, "writeError")
+}
+
+
+func main(){
 	 TCP_hookup("129.241.187.149:33501")
 }
 
