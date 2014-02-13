caa8499a4c549c2d2e20ad9443ebd153ba401009
Henrik Meland <melandhenrik@gmail.com>
2014-02-13 10:54:03 +0100

    småfiksis

--- Prosjekt/driver.go
+++ Prosjekt/driver.go
@@ -4,13 +4,13 @@ import (
 	"time"
 )
 
-func func main() {
+func main() {
 	IoInit()
 	SetBit(MOTORDIR)
 	WriteAnalog(MOTOR, 100)
-	sleep(2*time.Second)
-	WriteAnalog(Motor,-100)
-	sleep(2*time.Second)
-	WriteAnalog(Motor,0)
+	sleep(2 * time.Second)
+	WriteAnalog(Motor, -100)
+	sleep(2 * time.Second)
+	WriteAnalog(Motor, 0)
 	ClearBit(MOTORDIR)
 }
