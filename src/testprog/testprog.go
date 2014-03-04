ea0ee15edca352ba8b46ce8be242f0e8e3a7a9b0
Henrik Meland <melandhenrik@gmail.com>
2014-03-04 18:33:57 +0100

    Revert e3c0c5c..04a9c20
    This rolls back to commit e3c0c5c70686754ccf17b3d1a11f5b87681d4473.

--- src/testprog/testprog.go
+++ src/testprog/testprog.go
@@ -1,28 +1,22 @@
 package main
 
 import (
-	"network"
-	//"driver"
+	"driver"
 	"fmt"
-	//"time"
+	"time"
 )
 
 func main() {
-	/*go func(){
 	driver.IoInit()
 	driver.SetBit(driver.LIGHT_STOP)
 	driver.SetBit(driver.LIGHT_COMMAND1)
 	driver.SetBit(driver.LIGHT_DOWN2)
 	driver.SetBit(driver.MOTORDIR)
 	driver.WriteAnalog(driver.MOTOR, 4000)
-	fmt.Println("Going to sleep.")
 	time.Sleep(1 * time.Second)
-	fmt.Println("Waking.")
 	driver.ClearBit(driver.MOTORDIR)
 	driver.WriteAnalog(driver.MOTOR, 4000)
-	fmt.Println("Going to sleep.")
 	time.Sleep(1 * time.Second)
-	fmt.Println("Waking.")
 	driver.WriteAnalog(driver.MOTOR, 0)
 	driver.ClearBit(driver.MOTORDIR)
 
@@ -31,17 +25,5 @@ func main() {
 	driver.ClearBit(driver.LIGHT_DOWN2)
 
 	driver.IoInit()
-	}()
-	*/
-	alivePort := "33042"
-	incoming := make(chan string)
 
-	go network.ImAliveUDP(alivePort)
-	go network.ListenToNetworkUDP(network.MakeListenerConn(alivePort), incoming)
-
-	for{
-		fmt.Println(<-incoming, "bgerboeg")
-		}
-	var exit string
-	fmt.Scanln(&exit)
 }
