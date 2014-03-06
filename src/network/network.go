c4754f1c7f4d9f0f481adedbc087ac96a78e9943
Henrik Meland <melandhenrik@gmail.com>
2014-03-06 14:23:03 +0100

    jobbet på nettverk

--- src/network/network.go
+++ src/network/network.go
@@ -5,11 +5,24 @@ import (
 	"net"
 	"time"
 	"strings"
+	"encoding/json"
+	"queue"
 )
 
 const N_ELEVATORS int = 2
 
+const (
+	ORDERPORT string = "44001"
+	ALIVEPORT string = "44002"
+	COSTPORT string = "44003"
+)
+
 type AliveArray [][]string
+
+type TestVariable struct{
+	Kek int 
+	Lol  string 
+}
 
 func CheckError(err error, errorMsg string) {
 	if err != nil {
@@ -17,9 +30,9 @@ func CheckError(err error, errorMsg string) {
 	}
 }
 
-func ImAliveUDP(port string) {
+func ImAliveUDP() {
 	fmt.Println("Establishing IAmAlive...")
-	sendAddr, err := net.ResolveUDPAddr("udp4", "129.241.187.255:"+port)
+	sendAddr, err := net.ResolveUDPAddr("udp4", "129.241.187.255:"+ALIVEPORT)
 	CheckError(err, "ERROR while resolving UDP addr")
 	conn, err := net.DialUDP("udp4", nil, sendAddr)
 	CheckError(err, "ERROR while dialing")
@@ -30,12 +43,12 @@ func ImAliveUDP(port string) {
 	}
 }
 
-func RecieveAliveUDP(alivePort string, aliveChan *chan map[string]time.Time){
+func RecieveAliveUDP(aliveChan *chan string){
 	data := make([]byte, 1024)
 	ownAddr := *GetLocalIp();
-	conn := network.MakeListenerConn(alivePort)
+	conn := MakeListenerConn(ALIVEPORT)
 	for {		
-		_, addr_, err := conn.ReadFromUDP(data)
+		_, addr, err := conn.ReadFromUDP(data)
 		CheckError(err, "ERROR ReadFromUDP")
 
 		if (string(data) == "ImAlive!") && (addr.String() != ownAddr){
@@ -46,11 +59,14 @@ func RecieveAliveUDP(alivePort string, aliveChan *chan map[string]time.Time){
 }
 
 func UpdateAliveUDP(aliveChan *chan string, updateChan *chan map[string]time.Time) {
-	for {
+	aliveMap := make(map[string]time.Time)
+	var lengthOfMap int = 0
+
+	for {	
 		select{
 			case incomingIP := <-*aliveChan:
 				aliveMap[incomingIP] = time.Now()
-			case <-updateChan:
+			case <-*updateForCostChan:
 				*updateChan<-aliveMap
 			default:
 				for i, value := range aliveMap {//Iterate through alive-map and delete timed-out machines
@@ -66,7 +82,11 @@ func UpdateAliveUDP(aliveChan *chan string, updateChan *chan map[string]time.Tim
 	}
 }
 
-func SendToNetworkUDP(port string, msg string) {
+func AliveOrDead(){
+
+}
+
+func SendUDP(port string, msg string) {
 	sendAddr, err := net.ResolveUDPAddr("udp4", "129.241.187.255:"+port)
 	CheckError(err, "ERROR while resolving UDP addr")
 	conn, err := net.DialUDP("udp4", nil, sendAddr)
@@ -81,7 +101,7 @@ func GetLocalIp() *string {
 	return &strings.Split(conn.LocalAddr().String(), ":")[0]
 }
 
-func ListenToNetworkUDP(conn *net.UDPConn, incoming *chan string) {
+func ListenToOrderUDP(conn *net.UDPConn, incoming *chan string) {
 	data := make([]byte, 1024)
 	//ownAddr := *GetLocalIp();
 	for {
@@ -98,7 +118,81 @@ func ListenToNetworkUDP(conn *net.UDPConn, incoming *chan string) {
 	conn.Close()
 }
 
+func RecieveOrder(orderChannel chan queue.Order) {
+	conn := MakeListenerConn(ORDERPORT)
+	data := make([]byte, 1024)
+	for {
+		_, _, err := conn.ReadFromUDP(data)
+		CheckError(err, "ERROR ReadFromUDP")
 
+		var newOrder queue.Order
+		json.Unmarshal(newOrder, &data)
+		go func(order Order, orderChannel chan Order){
+			orderChannel <- newOrder
+		}(newOrder)
+
+	}
+	//motta ordre
+	//videresend ordre
+	//bekreft mottat ordre
+}
+
+func SendOrder(order Order){
+	sendAddr, err := net.ResolveUDPAddr("udp4", "129.241.187.255:"+ORDERPORT)
+	CheckError(err, "ERROR while resolving UDP addr")
+	conn, err := net.DialUDP("udp4", nil, sendAddr)
+	CheckError(err, "ERROR while dialing")
+
+	for{
+	orderB,_ := json.Marshal(order)
+	conn.Write([]byte(order))
+
+	if RecieveCost(){
+		break
+	}
+	//Motta cost fra alle
+	//Hvis ikke cost mottas fra alle, oppdater koblingsliste og send på nytt
+	}
+
+	
+	conn.Close()
+}
+
+func SendCost() {
+	
+}
+
+func RecieveCost() bool{
+	conn := MakeListenerConn(COSTPORT)
+
+	dummy := make(map[string]time.Time)
+	updateForCostChan <- dummy
+	aliveMap <- updateForCostChan
+
+	costMap := make(map[string]int)
+
+	//Les UDP i ett sekund eller til alle har levert cost rapport
+	costInstance := make(queue.Cost)
+	data := make([]byte, 1024)
+	for {
+		
+		_,err := net.ReadFromUDP(data)
+		json.Unmarshal(costInstance, &data)
+
+
+
+	}
+	//Lag et costMap med det som leses på UDP - Hvor mange ganger skal det leses over UDP? Lese kontinuerlig over en viss periode?
+	//Det skal være like langt som IP-listen
+	//Dersom vi har en cost for alle IP kan vi gå videre
+	
+
+	conn.close()
+}
+
+func confirmOrder() {
+	
+}
 
 func MakeListenerConn(port string) *net.UDPConn{
 	udpAddr, err := net.ResolveUDPAddr("udp4", ":"+port)
